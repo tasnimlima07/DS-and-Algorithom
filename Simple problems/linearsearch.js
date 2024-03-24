4e30f2c9a2e29f4e01d9fa2a5b66774666392e2e
tasnimlima07 <98936611+tasnimlima07@users.noreply.github.com>
2024-03-24 21:24:17 +0600

    Update linearsearch.js

--- Simple problems/linearsearch.js
+++ Simple problems/linearsearch.js
@@ -1,1 +1,14 @@
+let arr = [1, 2, 3, 5, 6];
+let n = 5;
+
+function myFunc(arr, n) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == n) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+console.log(myFunc(arr, n)); 
 
