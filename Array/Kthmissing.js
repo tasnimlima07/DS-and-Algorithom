57692f285e24d2fd5c7b4875900c0e3b8fe0c8d2
tasnimlima07 <98936611+tasnimlima07@users.noreply.github.com>
2024-03-25 11:13:59 +0600

    Update Kthmissing.js
    Kth missing in js

--- Array/Kthmissing.js
+++ Array/Kthmissing.js
@@ -1,1 +1,35 @@
+/*
+Example 1:
 
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let list=[];
+    let arr1=[];
+    for(let i=1; i<=100000; i++){
+        arr1.push(i)
+    }
+    let newArr = arr1.filter(item => !arr.includes(item));
+    return newArr[k-1]
+};
