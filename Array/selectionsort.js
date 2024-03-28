2fc7fc7270132ccae9469f8b78c5e27e8fa70760
tasnimlima07 <98936611+tasnimlima07@users.noreply.github.com>
2024-03-28 21:10:10 +0600

    Update selectionsort.js

--- Array/selectionsort.js
+++ Array/selectionsort.js
@@ -1,19 +1,57 @@
-const nums = [2, 4, 6, 8,1, 10, 12, 14, 16];
+/*912. Sort an Array(Leet code problem)
+Solved
+Medium
+Topics
+Companies
+Given an array of integers nums, sort the array in ascending order and return it.
 
-function compareNumbers(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let lowest = i;
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] < nums[lowest]) {
-                lowest = j;
-            }
-        }
-        if (lowest !== i) {
-            [nums[i], nums[lowest]]=[nums[lowest],nums[i]]
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104 
+*/
+
+function sortArray(nums) {
+    const mp = new Map();
+
+    for (const num of nums) {
+        mp.set(num, (mp.get(num) || 0) + 1);
+    }
+
+    const minE = Math.min(...nums);
+    const maxE = Math.max(...nums);
+
+    let i = 0;
+
+    for (let num = minE; num <= maxE; num++) {
+        while (mp.get(num) > 0) {
+            nums[i] = num;
+            i++;
+            mp.set(num, mp.get(num) - 1);
         }
     }
+
     return nums;
 }
 
-const result = compareNumbers(nums);
-console.log(result);
+// Example usage:
+const nums = [5, 2, 3, 1, 2];
+console.log(sortArray(nums)); // Output: [1, 2, 3, 5]
+
+
