5b111adc3453544668549341f35fc89f4d6f7de6
tasnimlima07 <98936611+tasnimlima07@users.noreply.github.com>
2024-03-28 12:52:09 +0600

    Update binerySearch.js

--- Array/binerySearch.js
+++ Array/binerySearch.js
@@ -1,1 +1,22 @@
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
 
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid; 
+        } else if (arr[mid] < target) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+    
+    return -1;
+}
+
+const arr = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12];
+const target = 7;
+const index = binarySearch(arr, target);
+console.log(index)
